b295782a69aab747407d0e97aba9a38aeb046f4a
rvdang256 <69442119+rvdang256@users.noreply.github.com>
2023-12-02 20:22:01 -0500

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,122 @@
+from keras.datasets import fashion_mnist
+import numpy as np
+import tensorflow as tf
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Input
+from keras.layers import LeakyReLU
+from sklearn.metrics import classification_report
+
+
+(train_X,train_Y), (test_X,test_Y) = fashion_mnist.load_data()
+
+
+#Data Preprocesssing
+train_X = train_X.reshape(-1, 28,28, 1)
+test_X = test_X.reshape(-1, 28,28, 1)
+
+train_X = train_X.astype('float32')
+test_X = test_X.astype('float32')
+train_X = train_X / 255.
+test_X = test_X / 255.
+
+# Change the labels from categorical to one-hot encoding
+train_Y_one_hot = to_categorical(train_Y)
+test_Y_one_hot = to_categorical(test_Y)
+
+#Split the data into training and validation
+train_X,valid_X,train_label,valid_label = train_test_split(train_X, train_Y_one_hot, test_size=0.3, random_state=4)
+
+
+
+
+batch_size = 128
+epochs = 20
+num_classes = 10
+
+#All the different layers of the model
+fashion_model = Sequential()
+fashion_model.add(Input(shape=(28,28,1)))
+fashion_model.add(Conv2D(128, kernel_size=(3, 3),activation='linear',padding='same'))
+fashion_model.add(LeakyReLU(alpha=0.1))
+fashion_model.add(MaxPooling2D((2, 2),padding='same'))
+fashion_model.add(Dropout(0.25))
+fashion_model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+fashion_model.add(LeakyReLU(alpha=0.1))
+fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+fashion_model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+fashion_model.add(LeakyReLU(alpha=0.1))                
+fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+fashion_model.add(Dropout(0.25))
+fashion_model.add(Flatten())
+fashion_model.add(Dense(128, activation='linear'))
+fashion_model.add(LeakyReLU(alpha=0.1)) 
+fashion_model.add(Dropout(0.25))             
+fashion_model.add(Dense(num_classes, activation='softmax'))
+
+
+optimizer = keras.optimizers.Adam()
+fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=optimizer,metrics=['accuracy'])
+
+
+
+fashion_model.summary()
+
+
+def scheduler(epoch, lr):
+      if epoch < 8:
+        return lr
+      else:
+        return lr * tf.math.exp(-0.1)
+      
+my_callbacks = [
+    tf.keras.callbacks.EarlyStopping(monitor="val_loss",patience=2),
+    tf.keras.callbacks.LearningRateScheduler(scheduler)
+]
+
+fashion_train = fashion_model.fit(train_X, train_label, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_label), callbacks=my_callbacks)
+
+test_eval = fashion_model.evaluate(test_X, test_Y_one_hot, verbose=0)
+
+#Test Loss and Test Accuracy
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
+
+
+
+#Confusion Matrix
+predicted_classes = fashion_model.predict(test_X)
+predicted_classes = np.argmax(np.round(predicted_classes),axis=1)
+print(confusion_matrix(test_Y, predicted_classes))
+
+
+
+#Classification Report
+target_names = ["Class {}".format(i) for i in range(num_classes)]
+print(classification_report(test_Y, predicted_classes, target_names=target_names))
+
+#Leanring Curves
+accuracy = fashion_train.history['accuracy']
+epochs = range(len(accuracy))
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, fashion_train.history['val_accuracy'], 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+
+plt.plot(epochs, fashion_train.history['loss'], 'bo', label='Training loss')
+plt.plot(epochs, fashion_train.history['val_loss'], 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+
+
+
